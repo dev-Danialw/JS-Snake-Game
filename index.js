cfd14505227ff7aabf6406078e0f4e9c98cb151c
Danial-Waheed <daniyalwahied@gmail.com>
2022-03-04 21:15:18 +0500

    added restarting functionality

--- index.js
+++ index.js
@@ -32,6 +32,20 @@ currentSnake.forEach((index) => squares[index].classList.add("snake"));
 
 // starting game;
 function startGame() {
+  // remove the snake from the grid
+  currentSnake.forEach((index) => squares[index].classList.remove("snake"));
+  // remove the apple
+  squares[appleIndex].classList.remove("apple");
+  clearInterval(timerId);
+  currentSnake = [2, 1, 0];
+  score = 0;
+  // readd the score on the screen
+  scoreDisplay.textContent = score;
+  direction = 1;
+  intervalTime = 1000;
+  generateApple();
+  // readd the class of snake to our new currentSnake
+  currentSnake.forEach((index) => squares[index].classList.add("snake"));
   timerId = setInterval(move, intervalTime);
 }
 
