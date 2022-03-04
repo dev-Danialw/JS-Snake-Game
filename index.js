3c8407d22468f0577a576c6400d15041c4efca72
Danial-Waheed <daniyalwahied@gmail.com>
2022-03-04 14:23:53 +0500

    added squares into grid using js

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 const grid = document.querySelector(".grid");
 const startBtn = document.getElementById("start");
 const score = document.getElementById("score");
-
+let squares = [];
 // creating grid
-function createGrid(params) {}
+function createGrid() {
+  // 100 of these elements with a loop (for)
+  for (let i = 0; i < 100; i++) {
+    // create elements
+    const square = document.createElement("div");
+    // add styles to the element
+    square.classList.add("square");
+    // put the element into our grid
+    grid.appendChild(square);
+    // push it into a new squares array
+    squares.push(square);
+  }
+}
+createGrid();
