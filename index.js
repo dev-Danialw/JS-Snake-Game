47a7bfb75bfd1eb0107bfb1108ef08285ee6b540
Danial-Waheed <daniyalwahied@gmail.com>
2022-03-04 14:14:11 +0500

    picked dom elements

---
+++ index.js
@@ -0,0 +1,7 @@
+//  Grabbing the DOM elements
+const grid = document.querySelector(".grid");
+const startBtn = document.getElementById("start");
+const score = document.getElementById("score");
+
+// creating grid
+function createGrid(params) {}
